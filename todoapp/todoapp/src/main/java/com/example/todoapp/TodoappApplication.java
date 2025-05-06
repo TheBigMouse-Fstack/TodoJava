54a2f61f14d8d790e4492b05c43e322addd77e10
TheBigMouse-MernStack <khanhnqse19@gmail.com>
2025-05-06 11:51:40 +0700

    change folder

--- todoapp/todoapp/src/main/java/com/example/todoapp/TodoappApplication.java
+++ todoapp/todoapp/src/main/java/com/example/todoapp/TodoappApplication.java
@@ -1,13 +1,13 @@
-package com.example.todo;
+package com.example.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
+		SpringApplication.run(TodoappApplication.class, args);
 	}
 
 }
--- todoapp/todoapp/src/test/java/com/example/todoapp/TodoappApplicationTests.java
+++ todoapp/todoapp/src/test/java/com/example/todoapp/TodoappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.todo;
+package com.example.todoapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TodoApplicationTests {
+class TodoappApplicationTests {
 
 	@Test
 	void contextLoads() {
